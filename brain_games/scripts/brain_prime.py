27771799e132b5f9b3da187fc93a00b7b0c46dd7
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2024-01-09 21:37:13 +0300

    add prime game

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import random
+from brain_games.game import brain_game
+
+RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(value):
+    if value <= 1:
+        return False
+    elif value > 1:
+        for i in range(2, int(value / 2) + 1):
+            if value % i == 0:
+                return False
+    return True
+
+
+def get_question_answer():
+    question = random.randint(1, 300)
+    answer = 'yes' if is_prime(question) else 'no'
+    return [question, answer]
+
+
+def main():
+    brain_game(get_question_answer, RULE)
+
+
+if __name__ == '__main__':
+    main()
