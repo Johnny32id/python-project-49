e7d8703ea2392888fda69c4a370c496fd69fcc76
Evgeniy Slizh <johnnyhandsome3232@gmail.com>
2024-01-09 19:49:02 +0300

    add calc game

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import random
+from brain_games.game import brain_game
+
+RULE = 'What is the result of the expression?'
+
+
+def get_random_sign(signs):
+    random_index = random.randint(0, len(signs) - 1)
+    return signs[random_index]
+
+
+def calculate(first_value, second_value, sign):
+    match sign:
+        case '+':
+            return first_value + second_value
+        case '-':
+            return first_value - second_value
+        case _:
+            return first_value * second_value
+
+
+def get_question_answer():
+    first_value = random.randint(1, 100)
+    second_value = random.randint(1, 100)
+    random_sign = get_random_sign(['+', '-', '*'])
+    question = f"{first_value} {random_sign} {second_value}"
+    answer = str(calculate(first_value, second_value, random_sign))
+    return [question, answer]
+
+
+def main():
+    brain_game(get_question_answer, RULE)
+
+
+if __name__ == '__main__':
+    brain_game()
