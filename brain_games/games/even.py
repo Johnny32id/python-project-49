bb8dfeeebfbdd7fe7cd01c9c8277718124a3e135
Johnny32id <johnnyhandsome3232@gmail.com>
2024-01-19 20:52:26 +0300

    separated func

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import random
+import brain_games.check_yes_or_no
 
 RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
@@ -10,5 +11,5 @@ def is_even(value):
 
 def get_question_answer():
     question = random.randint(1, 100)
-    answer = 'yes' if is_even(question) else 'no'
+    answer = brain_games.check_yes_or_no.check(question, is_even)
     return [question, answer]
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import random
+import brain_games.check_yes_or_no
 
 RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
@@ -16,5 +17,5 @@ def is_prime(value):
 
 def get_question_answer():
     question = random.randint(1, 300)
-    answer = 'yes' if is_prime(question) else 'no'
+    answer = brain_games.check_yes_or_no.check(question, is_prime)
     return [question, answer]
